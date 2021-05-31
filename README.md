0e81a8e1fd1f281609019f4f766ae4ec314aeb42
Jesse Claven <jesse.claven@me.com>
2021-05-31 22:30:21 +0100

    Switch to Swift Package

---
+++ README.md
@@ -0,0 +1,3 @@
+# AppearanceNotifier
+
+A description of this package.
---
+++ Sources/AppearanceNotifier/main.swift
@@ -0,0 +1,51 @@
+import AppKit
+import Foundation
+import ShellOut
+
+private let kAppleInterfaceThemeChangedNotification = "AppleInterfaceThemeChangedNotification"
+
+enum Theme: String {
+    case Light, Dark
+}
+
+class DarkModeObserver {
+    func observe() {
+        print("Observing")
+
+        DistributedNotificationCenter.default.addObserver(
+            forName: Notification.Name(kAppleInterfaceThemeChangedNotification),
+            object: nil,
+            queue: nil,
+            using: interfaceModeChanged(notification:)
+        )
+    }
+
+    func interfaceModeChanged(notification _: Notification) {
+        let styleRaw = UserDefaults.standard.string(forKey: "AppleInterfaceStyle") ?? "Light"
+        let style = Theme(rawValue: styleRaw)!
+
+        print("Theme changed: \(style)")
+
+        do {
+            let output = try shellOut(to: "echo", arguments: ["Hello world"])
+            print(output) // Hello world
+        } catch {
+            let error = error as! ShellOutError
+            print(error.message) // Prints STDERR
+            print(error.output) // Prints STDOUT
+        }
+    }
+}
+
+let app = NSApplication.shared
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_: Notification) {
+        let observer = DarkModeObserver()
+        observer.observe()
+    }
+}
+
+let delegate = AppDelegate()
+app.delegate = delegate
+app.run()
