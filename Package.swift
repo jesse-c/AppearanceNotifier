d9280ceb47de003d4381b0cf08e1cd6aa8af70a7
Jesse Claven <jesse.claven@me.com>
2021-06-09 22:42:57 +0100

    Remove autogenerated comments

--- Package.swift
+++ Package.swift
@@ -1,18 +1,13 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AppearanceNotifier",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppearanceNotifier",
             dependencies: ["ShellOut"]
